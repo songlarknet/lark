b5bd2440c0d451aab74853e76060ad32c2ba2244
Amos Robinson <amos.robinson@gmail.com>
2022-09-20 16:54:01 +1000

    minor clean

--- src/main/scala/lack/meta/source/stream.scala
+++ src/main/scala/lack/meta/source/stream.scala
@@ -20,36 +20,32 @@ object stream:
 
   class SortRepr[T](val sort: Sort)
 
-  // TODO: remove this, unnecessary
-  object internal:
-    case class Prim[T <: Sort]()
-
   opaque type Bool    = Sort.Bool.type
   given SortRepr_Bool: SortRepr[Bool] = new SortRepr(Sort.Bool)
 
-  opaque type Int8    = internal.Prim[Sort.Int8.type]
+  opaque type Int8    = Sort.Int8.type
   given SortRepr_Int8: SortRepr[Int8] = new SortRepr(Sort.Int8)
 
-  opaque type UInt8   = internal.Prim[Sort.UInt8.type]
+  opaque type UInt8   = Sort.UInt8.type
   given SortRepr_UInt8: SortRepr[UInt8] = new SortRepr(Sort.UInt8)
 
-  opaque type Int16   = internal.Prim[Sort.Int16.type]
+  opaque type Int16   = Sort.Int16.type
   given SortRepr_Int16: SortRepr[Int16] = new SortRepr(Sort.Int16)
 
-  opaque type UInt16  = internal.Prim[Sort.UInt16.type]
+  opaque type UInt16  = Sort.UInt16.type
   given SortRepr_UInt16: SortRepr[UInt16] = new SortRepr(Sort.UInt16)
 
-  opaque type Int32   = internal.Prim[Sort.Int32.type]
+  opaque type Int32   = Sort.Int32.type
   given SortRepr_Int32: SortRepr[Int32] = new SortRepr(Sort.Int32)
 
-  opaque type UInt32  = internal.Prim[Sort.UInt32.type]
+  opaque type UInt32  = Sort.UInt32.type
   given SortRepr_UInt32: SortRepr[UInt32] = new SortRepr(Sort.UInt32)
 
-  opaque type Int64   = internal.Prim[Sort.Int64.type]
+  opaque type Int64   = Sort.Int64.type
   given SortRepr_Int64: SortRepr[Int64] = new SortRepr(Sort.Int64)
 
-  opaque type UInt64  = internal.Prim[Sort.UInt64.type]
+  opaque type UInt64  = Sort.UInt64.type
   given SortRepr_UInt64: SortRepr[UInt64] = new SortRepr(Sort.UInt64)
 
-  opaque type Real32 = internal.Prim[Sort.Real32.type]
+  opaque type Real32 = Sort.Real32.type
   given SortRepr_Real32: SortRepr[Real32] = new SortRepr(Sort.Real32)
